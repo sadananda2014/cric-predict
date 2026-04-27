6038fc3e7444b70008041848859cfdf56308327a
Sadananda <sadananda2014@gmail.com>
2026-04-27 15:48:43 +0530

    removed the PWA settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Local dev: /   |   GitHub Pages: /cric-predict/
-  base: process.env.NODE_ENV === 'production' ? '/cric-predict/' : '/',
+  base: '/',
   plugins: [react()],
 })
